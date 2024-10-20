0303c71a1e9eb6111bd19212d65c5d65129348ef
Atond <max_max_max5@hotmail.fr>
2024-10-20 21:43:28 +0200

    Changes to be committed: 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -104,50 +104,6 @@ function addLoot(loot) {
     updateInventoryDisplay(currentTranslations);
 }
 
-function getResourceData(resourceId) {
-    return resourceManager.getResource(resourceId);
-}
-
-export function updateInventoryDisplay(translations) {
-    const inventoryElement = document.getElementById('profession-inventory');
-    const allItems = playerInventory.getAllItems();
-    const totalItems = Object.keys(allItems).length;
-    const totalPages = Math.ceil(totalItems / itemsPerPage);
-
-    inventoryElement.innerHTML = '';
-
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
-    const itemsToDisplay = Object.entries(allItems).slice(startIndex, endIndex);
-
-    for (const [resourceId, count] of itemsToDisplay) {
-        const resource = getResourceData(resourceId);
-        if (resource) {
-            const slot = document.createElement('div');
-            slot.className = 'inventory-slot';
-            slot.innerHTML = `
-                <img src="${resource.image}" alt="${translations.resources[resourceId]}">
-                <div class="item-count">${count}</div>
-                <div class="tooltip">${translations.resources[resourceId]}</div>
-            `;
-            inventoryElement.appendChild(slot);
-        }
-    }
-
-    const paginationElement = document.getElementById('pagination');
-    paginationElement.innerHTML = '';
-
-    for (let i = 1; i <= totalPages; i++) {
-        const pageButton = document.createElement('button');
-        pageButton.innerText = i;
-        pageButton.addEventListener('click', () => {
-            currentPage = i;
-            updateInventoryDisplay(translations);
-        });
-        paginationElement.appendChild(pageButton);
-    }
-}
-
 export function loadTranslations(language) {
     return fetch(`translations/${language}.json`)
         .then(response => response.json())
